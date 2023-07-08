34f10bde81cecb5a6a760ef1cb6de069c6334d09
Autumn <autumn.chiu.136@gmail.com>
2023-07-08 02:45:47 -0400

    browser version

---
+++ app.js
@@ -0,0 +1,142 @@
+const game = {
+    player: {
+	inventory: [],
+        getNewItems: function(n) {
+            for (let i = 0; i < n; i++) {
+	        game.player.inventory.push({
+	            name: randItem(text.adjectives) + " " + randItem(text.nouns),
+	            rating: 1 + Math.floor(Math.random() * game.options.item_max_rating),
+	        })
+            }
+        }
+    },
+    score: {
+	rebels: 0,
+	loyalists: 0,
+    },
+    options: {
+	// total more than 10 breaks game because ordinals
+	rebels_per_day: 2,
+	loyalists_per_day: 2,
+	random_per_day: 2,
+
+	days_to_win: 3,
+
+	stutter_chance: 0.7,
+	compromised_chance: 0.3,
+
+	item_max_rating: 10,
+	items_initial: 7,
+	items_min: 4,
+	items_new: 4,
+    }
+}
+game.player.getNewItems(game.options.items_initial)
+
+print(text.instructions.replace("\n", "<br>"))
+button("Continue...", play)
+
+function play() {
+    clear()
+    function startDay() {
+        clear()
+        print("A new day begins.")
+        let day = {
+            count: 1,
+            code_word: randItem(text.code_words),
+            compromised: Math.random() < game.options.compromised_chance,
+            rebels: 0,
+            loyalists: 0,
+            rebels_count: game.options.rebels_per_day,
+            loyalists_count: game.options.loyalists_per_day,
+        }
+	for (let i = 0; i < game.options.random_per_day; i++) {
+	    if (Math.random() < 0.5) day.rebels_count += 1
+	    else day.loyalists_count += 1
+	}
+        function startAdventurer() {
+	    let adventurer_faction
+	    if (day.rebels_count === 0) {
+	        adventurer_faction = "loyalists";
+	        day.loyalists_count--;
+	    } else if (day.loyalists_count === 0) {
+	        adventurer_faction = "rebels";
+	        day.rebels_count--;
+	    } else {
+	        adventurer_faction = Math.random() < 0.5 ? "rebels" : "loyalists"
+	        if (adventurer_faction === "rebels") {
+		    day.rebels_count--;
+	        } else {
+		    day.loyalists_count--;
+	        }
+	    }
+	    let phrase
+	    if (!day.compromised) {
+	        if (adventurer_faction === "rebels") {
+		    phrase = randItem(text.cryptic_phrases)
+	        } else if (adventurer_faction === "loyalists") {
+		    phrase = randItem(text.cryptic_phrases)
+		    if (Math.random() < game.options.stutter_chance) {
+		        phrase = phrase.replace(" *", `, uh... ${randItem(text.code_words)}`)
+		    } else {
+		        phrase = phrase.replace("*", randItem(text.code_words))
+		    }
+	        }
+	    } else {
+	        if (adventurer_faction === "rebels") {
+		    phrase = randItem(text.compromise_phrases)
+	        } else if (adventurer_faction === "loyalists") {
+		    phrase = randItem(text.cryptic_phrases)
+	        }
+	    }
+	    phrase = phrase.replace("*", day.code_word)
+            print(`The ${text.ordinals[day.count]} adventurer walks into your shop.`)
+            print(`The adventurer speaks: "${phrase}"`)
+            print(`What item will you sell them?`)
+            game.player.inventory.forEach((x, i) => {
+                button(`${i}: ${x.name} (rating ${x.rating})`, () => {
+                    let item = game.player.inventory.splice(i, 1)[0]
+	            day[adventurer_faction] += item.rating
+                    clear()
+                    print(`You give the adventurer your ${item.name}.`)
+                    if (game.player.inventory.length <= game.options.items_min) {
+	                game.player.getNewItems(game.options.items_new)
+	                print("You get a new shipment of items.\n")
+	            }
+	            day.count++
+                    if (day.rebels_count > 0 || day.loyalists_count > 0) {
+                        startAdventurer(day)
+                    } else {
+                        endDay(day)
+                    }
+                })
+            })
+        } // startAdventurer
+        startAdventurer()
+    } // startDay
+    function endDay(day) {
+        print("The day comes to an end.")
+        if (day.rebels === day.loyalists) {
+	    print("The two sides have reached a bloody stalemate for the day.")
+        } else {
+	    let winners = day.rebels > day.loyalists ? "rebels" : "loyalists"
+            game.score[winners]++
+	    print(`The ${winners} have won the day with their superior equipment.`)
+        }
+        // game end
+        if (game.score.loyalists === game.options.days_to_win) {
+	    print("The loyalists have suppressed the rebellion. You pray they have not discovered your treachery.")
+        }
+        else if (game.score.rebels === game.options.days_to_win) {
+	    print("The rebels have overthrown the monarchy. Your country has a bright future ahead.")
+        }
+        else {
+            button("Continue...", startDay)
+        }
+    }
+    startDay()
+} // play
+
+function randItem(list) {
+    return list[Math.floor(Math.random() * list.length)];
+}
---
+++ text.js
@@ -0,0 +1,31 @@
+const output = document.getElementById("output")
+const input = document.getElementById("input")
+
+let text = {
+    "instructions": "A rebellion has begun to overthrow the corrupt monarchy. You are a shopkeeper sympathetic to the rebel cause; you must identify and server fellow rebel adventurers, while refusing to serve the nasty loyalists.\n\nThe rebels have a CODE WORD they use to identify each other that changes from day to day. Try to figure it out and give them the best equipment in your shop. But be advised that the Loyalists are trying to crack the code word, as well...\n",
+    "code_words": ["jackalopes", "horses", "dogs", "carnations", "lilies", "poppies", "carrots", "wheat", "onions", "Gambian sidling bushes"],
+    "cryptic_phrases": ["I saw some * outside today. They were very beautiful.", "Have you bought any * at the market lately?", "The king has taken a fancy to *. You should find some for him.", "I'm looking for some *; if you find any, let me know.", "I love the smell of * in the morning.", "If you're free later, I have some * for you to look at.", "Don't you think some * would be great right now?", "I'm a big fan of *. Don't tell my husband.", "Are you interested in some *? I know where you can get some.", "This is the perfect time for some *, don't you think?"],
+    "compromise_phrases": ["Don't buy any * today. They're spoiled.", "We got a shipment of * today, but we had to send them back.", "The * weren't outside today. Very unfortunate.", "The * have fled. The day is ruined.", "If you see any * today, don't approach them. They're poisonous."],
+    "adjectives": ["Bloody", "Cool", "Epic", "Angry", "Masterful", "Dextrous", "Aggressive", "Charming", "Novel", "Your Mom's"],
+    "nouns": ["Sword", "Shield", "Potion", "Bow", "Crossbow", "Axe", "Mace", "Chestplate", "Gauntlets", "Ribbon", "Laurel"],
+    "ordinals": ["zeroth", "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eight", "ninth", "tenth"]
+}
+
+function print(text) {
+    let p = document.createElement("p")
+    p.innerHTML = text
+    output.appendChild(p)
+}
+
+function clear() {
+    input.innerHTML = ""
+    output.innerHTML = ""
+}
+
+function button(text, callback) {
+    let btn = document.createElement("button")
+    btn.innerHTML = text
+    btn.onclick = callback
+    input.appendChild(btn)
+}
+
