535927871df890b5a85f0245f97496c780b308b2
autumn <autumn.chiu.136@gmail.com>
2023-07-08 14:52:32 -0400

    Update text.js

--- text.js
+++ text.js
@@ -4,7 +4,7 @@ const input = document.getElementById("input")
 let text = {
     "instructions": [
         "A Rebellion is underway to overthrow the corrupt monarchy. You are a shopkeeper sympathetic to the Rebel cause, and have been tasked with supplying Rebel adventurers for their missions.",
-        "Every day, the Rebels will decide on a new, secret code word; Your job is to figure out the code word, and supply the Rebels who use it with your best equipment.",
+        "Every day, the Rebels will decide on a new, secret code word; your job is to figure out the code word, and supply the Rebels who use it with your best equipment.",
         "A true Rebel will always know the code word; a Loyalist will attempt to guess it, but may stutter or falter in their speech. Pay attention to words that come up frequently, and who speaks with confidence versus who's just guessing.",
         "The Loyalists are always working to crack the code, and some days the code word will be compromised. Your fellow Rebels will try to communicate this to you discreetly; pay attention to what they have to say.",
         "Give your best equipment to your Rebel allies and your worst to the Loyalist scum, and hope that the Rebels succeed in their missions and overthrow the monarchy."
